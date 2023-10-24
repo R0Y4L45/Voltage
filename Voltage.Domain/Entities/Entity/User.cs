a2e028d5b3eb14f3a5d24e374100203e200be982
R0Y4L45 <royalb9514@gmail.com>
2023-10-24 20:43:52 +0400

    Add project files.

---
+++ Voltage.Domain/Entities/Entity/User.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Voltage.Entities.Entity;
+
+public class User : IdentityUser
+{
+    public string? Photo { get; set; }
+}
---
+++ Voltage/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Voltage.Models;
+
+namespace Voltage.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
