8adbcf3a3abdbbac4974cdbe88809e697f049085
R0Y4L45 <royalb9514@gmail.com>
2023-10-27 13:44:37 +0400

    Update 2.4.1 -Hubs modified.

--- Voltage/Hubs/MessageHub.cs
+++ Voltage/Hubs/MessageHub.cs
@@ -4,5 +4,14 @@ namespace Voltage.Hubs;
 
 public class MessageHub : Hub
 {
-
+    public async Task SendMessage(string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", Context.User!.Identity!.Name, message);
+        await Console.Out.WriteLineAsync();
+    }
+    public async Task SendToUser(string userId, string message)
+    {
+        await Clients.User(userId).SendAsync("ReceiveMessage", Context.User!.Identity!.Name, message);
+    }
+    public string GetConnectionId() => Context.UserIdentifier!;
 }
