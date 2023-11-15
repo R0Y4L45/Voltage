517306f5f469199bee58f3e1e817d11a8da5c4f9
R0Y4L45 <106491312+R0Y4L45@users.noreply.github.com>
2023-11-15 17:26:02 +0400

    Update 4.1.7 -Message js file updated

--- Voltage/wwwroot/js/signalRClientSide.js
+++ Voltage/wwwroot/js/signalRClientSide.js
@@ -1,11 +1,14 @@
-﻿let connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").withAutomaticReconnect().build(),
-    curUser, curUserId, recUserId, list = document.getElementById("messagesList");
+﻿let connection = curUser = curUserId = recUserId = null, list = document.getElementById("messagesList");
+
+//#region SignalR Connection and its events 
+
+connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").withAutomaticReconnect().build()
 
 connection.on("ReceiveMessage", (user, message) => {
     let encodedMsg = user + ": " + message;
 
     if (curUserId !== recUserId)
-        MessageCreater(encodedMsg, "left-item");
+        MessageCreater(encodedMsg, "message recipient");
 });
 
 connection.start().then(() => {
@@ -17,6 +20,9 @@ connection.start().then(() => {
     connection.invoke("GetConnectionId").then(id => curUserId = id);
 }).catch(err => console.error(err.toString()));
 
+//#endregion
+
+//#region Events
 document.getElementById("sendButton").addEventListener("click", function (event) {
     let message = document.getElementById("messageInput").value;
     connection.invoke("SendMessage", message).catch(err => console.error(err.toString()));
@@ -25,17 +31,17 @@ document.getElementById("sendButton").addEventListener("click", function (event)
 });
 
 document.getElementById("sendToUser").addEventListener("click", async event => {
-    let message = document.getElementById("messageInput").value;
-    connection.invoke("SendToUser", recUserId, message).catch(err => console.error(err.toString()));
-
-    await MessageSaver(message, curUserId, recUserId);
-    MessageCreater(curUser + ": " + message, "right-item");
-
-    //Complete it
-    //message.value = null;
-    event.preventDefault();
+    await SendMessage(event)
 });
 
+document.getElementById("messageInput").addEventListener("keypress", async event => {
+    if (event.key === "Enter")
+        await SendMessage(event)
+});
+
+//#endregion
+
+//#region Api's
 async function ClickToUser(username) {
     recUserId = await fetch('GetUserId', {
         method: 'POST',
@@ -51,8 +57,8 @@ async function ClickToUser(username) {
     arr = document.getElementById("messagesList");
     arr.innerText = "";
     (await FetchGetList(username)).forEach(i => i.sender == curUser
-        ? MessageCreater(i.sender + ': ' + i.content, "right-item")
-        : MessageCreater(i.sender + ': ' + i.content, "left-item"));
+        ? MessageCreater(i.sender + ': ' + i.content, "message sender")
+        : MessageCreater(i.sender + ': ' + i.content, "message recipient"));
 
     //Temporary created
     document.getElementById("receiverId").value = username;
@@ -90,10 +96,33 @@ async function MessageSaver(message, sender, receiver) {
         .catch(error => console.error(error));
 }
 
+//#endregion
+
+//#region HelperMethods
+async function SendMessage(event) {
+    let message = document.getElementById("messageInput").value;
+    if (recUserId !== null) {
+        connection.invoke("SendToUser", recUserId, message).catch(err => console.error(err.toString()));
+
+        await MessageSaver(message, curUserId, recUserId);
+        MessageCreater(curUser + ": " + message, "message sender");
+
+        document.getElementById("messageInput").value = '';
+        event.preventDefault();
+    }
+}
+
 function MessageCreater(message, style) {
     let li = document.createElement("li"), arr = document.getElementById("messagesList");
     li.textContent = message;
-    li.classList.add(style);
+    let a = style.split(' ');
+    a.forEach(s => {
+        if (message.length > 20)
+            li.classList.add("long-message")
+
+        li.classList.add(s)
+    });
 
     arr.appendChild(li);
 }
+//#endregion
