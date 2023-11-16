c080fd59b39a13c10b22c69a7d4b11a103b81b08
nihat417 <akreminihat@gmail.com>
2023-11-16 18:20:28 +0400

    Update 4.3.0 Change some methods in script Change style in messages

---
+++ Voltage/wwwroot/js/MainHeader.js
@@ -0,0 +1,33 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var menuItems = document.querySelectorAll('.navbar-nav .nav-item');
+    menuItems.forEach(function (menuItem) {
+        menuItem.addEventListener('click', function () {
+            menuItems.forEach(function (item) {
+                item.classList.remove('active');
+            });
+
+            menuItem.classList.add('active');
+        });
+    });
+
+    var lastActive = sessionStorage.getItem('lastActiveMenuItem');
+    if (lastActive) {
+        var activeElement = document.getElementById(lastActive);
+        if (activeElement) {
+            activeElement.classList.add('active');
+        }
+    }
+
+    document.addEventListener('click', function (event) {
+        var clickedElement = event.target.closest('.nav-item');
+        if (clickedElement) {
+            var itemId = clickedElement.id;
+            sessionStorage.setItem('lastActiveMenuItem', itemId);
+        }
+    });
+});
+
+function ToggleTheme(theme) {
+    var url = '@Url.Action("ToggleTheme", "Home")' + '?theme=' + theme;
+    window.location.href = url;
+}
--- Voltage/wwwroot/js/signalRClientSide.js
+++ Voltage/wwwroot/js/signalRClientSide.js
@@ -1,14 +1,33 @@
-﻿let connection = curUser = curUserId = recUserId = null, list = document.getElementById("messagesList");
+﻿//let connection = curUser = curUserId = recUserId = null, list = document.getElementById("messagesList");
+let connection = curUser = curUserId = recUserId = null, list = document.getElementById("messagesList");
+let author, date, avatarUrl;
 
-//#region SignalR Connection and its events 
+//#region SignalR Connection and its events
+
+//connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").withAutomaticReconnect().build()
+//
+//connection.on("ReceiveMessage", (user, message) => {
+//    let encodedMsg = user + ": " + message;
+//
+//    if (curUserId !== recUserId)
+//        MessageCreater(encodedMsg, "message recipient");
+//});
+//
+//connection.start().then(() => {
+//    connection.invoke("GetUserName").then(user => {
+//        document.getElementById("connectionId").innerText = user;
+//        curUser = user;
+//    });
+//
+//    connection.invoke("GetConnectionId").then(id => curUserId = id);
+//}).catch(err => console.error(err.toString()));
+
 
 connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").withAutomaticReconnect().build()
 
-connection.on("ReceiveMessage", (user, message) => {
-    let encodedMsg = user + ": " + message;
-
+connection.on("ReceiveMessage", (user, message, author, date, avatarUrl) => {
     if (curUserId !== recUserId)
-        MessageCreater(encodedMsg, "message recipient");
+        MessageCreater(message, "message recipient", author, date, avatarUrl);
 });
 
 connection.start().then(() => {
@@ -20,13 +39,36 @@ connection.start().then(() => {
     connection.invoke("GetConnectionId").then(id => curUserId = id);
 }).catch(err => console.error(err.toString()));
 
+
 //#endregion
 
 //#region Events
+
+//document.getElementById("sendButton").addEventListener("click", function (event) {
+//    let message = document.getElementById("messageInput").value;
+//    connection.invoke("SendMessage", message).catch(err => console.error(err.toString()));
+//
+//    event.preventDefault();
+//});
+//
+//document.getElementById("sendToUser").addEventListener("click", async event => {
+//    await SendMessage(event)
+//});
+//
+//document.getElementById("messageInput").addEventListener("keypress", async event => {
+//    if (event.key === "Enter")
+//        await SendMessage(event)
+//});
+
+
 document.getElementById("sendButton").addEventListener("click", function (event) {
     let message = document.getElementById("messageInput").value;
     connection.invoke("SendMessage", message).catch(err => console.error(err.toString()));
 
+    MessageSaver(message, curUserId, recUserId);
+    MessageCreater(curUser + ": " + message, "message sender", curUser, new Date().toLocaleTimeString(), avatarUrl);
+
+    document.getElementById("messageInput").value = '';
     event.preventDefault();
 });
 
@@ -39,9 +81,35 @@ document.getElementById("messageInput").addEventListener("keypress", async event
         await SendMessage(event)
 });
 
+
+
 //#endregion
 
 //#region Api's
+
+//async function ClickToUser(username) {
+//    recUserId = await fetch('GetUserId', {
+//        method: 'POST',
+//        headers: {
+//            'Content-Type': 'application/json'
+//        },
+//        body: JSON.stringify(username)
+//    })
+//        .then(response => response.json())
+//        .then(data => data)
+//        .catch(error => console.error(error));
+//
+//    arr = document.getElementById("messagesList");
+//    arr.innerText = "";
+//    (await FetchGetList(username)).forEach(i => i.sender == curUser
+//        ? MessageCreater(i.sender + ': ' + i.content, "message sender")
+//        : MessageCreater(i.sender + ': ' + i.content, "message recipient"));
+//
+//    //Temporary created
+//    document.getElementById("receiverId").value = username;
+//}
+
+
 async function ClickToUser(username) {
     recUserId = await fetch('GetUserId', {
         method: 'POST',
@@ -55,14 +123,60 @@ async function ClickToUser(username) {
         .catch(error => console.error(error));
 
     arr = document.getElementById("messagesList");
-    arr.innerText = "";
-    (await FetchGetList(username)).forEach(i => i.sender == curUser
-        ? MessageCreater(i.sender + ': ' + i.content, "message sender")
-        : MessageCreater(i.sender + ': ' + i.content, "message recipient"));
+    arr.innerHTML = "";
 
-    //Temporary created
+    const messages = await FetchGetList(username);
+
+    messages.forEach(message => {
+        MessageCreater(
+            message.content,
+            message.sender === curUser ? "message sender" : "message recipient",
+            message.sender,
+            message.date,
+            message.avatarUrl
+        );
+    });
+
     document.getElementById("receiverId").value = username;
 }
+
+
+
+
+//async function FetchGetList(receiver) {
+//    return await fetch('TakeMessages', {
+//        method: 'POST',
+//        headers: {
+//            'Content-Type': 'application/json'
+//        },
+//        body: JSON.stringify(receiver)
+//    })
+//        .then(response => response.json())
+//        .then(data => data)
+//        .catch(error => console.error(error));
+//}
+
+//async function MessageSaver(message, sender, receiver) {
+//    let object = {
+//        Content: message,
+//        Sender: sender,
+//        Receiver: receiver
+//    };
+//
+//    await fetch('AcceptMessage', {
+//        method: 'POST',
+//        headers: {
+//            'Content-Type': 'application/json'
+//        },
+//        body: JSON.stringify(object)
+//    })
+//        .then(response => response.json())
+//        .then(data => data)
+//        .catch(error => console.error(error));
+//}
+
+
+
 
 async function FetchGetList(receiver) {
     return await fetch('TakeMessages', {
@@ -73,7 +187,10 @@ async function FetchGetList(receiver) {
         body: JSON.stringify(receiver)
     })
         .then(response => response.json())
-        .then(data => data)
+        .then(data => data.map(message => ({
+            ...message,
+            style: message.sender === curUser ? "message sender" : "message recipient"
+        })))
         .catch(error => console.error(error));
 }
 
@@ -99,30 +216,88 @@ async function MessageSaver(message, sender, receiver) {
 //#endregion
 
 //#region HelperMethods
+
+//async function SendMessage(event) {
+//    let message = document.getElementById("messageInput").value;
+//    if (recUserId !== null) {
+//        connection.invoke("SendToUser", recUserId, message).catch(err => console.error(err.toString()));
+//
+//        await MessageSaver(message, curUserId, recUserId);
+//        MessageCreater(curUser + ": " + message, "message sender");
+//
+//        document.getElementById("messageInput").value = '';
+//        event.preventDefault();
+//    }
+//}
+
 async function SendMessage(event) {
     let message = document.getElementById("messageInput").value;
     if (recUserId !== null) {
         connection.invoke("SendToUser", recUserId, message).catch(err => console.error(err.toString()));
 
         await MessageSaver(message, curUserId, recUserId);
-        MessageCreater(curUser + ": " + message, "message sender");
+        MessageCreater(message, "message sender", curUser, new Date().toLocaleTimeString(), avatarUrl);
 
         document.getElementById("messageInput").value = '';
         event.preventDefault();
     }
 }
 
-function MessageCreater(message, style) {
-    let li = document.createElement("li"), arr = document.getElementById("messagesList");
-    li.textContent = message;
-    let a = style.split(' ');
-    a.forEach(s => {
-        if (message.length > 20)
-            li.classList.add("long-message")
 
-        li.classList.add(s)
-    });
 
-    arr.appendChild(li);
+
+//function MessageCreater(message, style) {
+//    let li = document.createElement("li"), arr = document.getElementById("messagesList");
+//    li.textContent = message;
+//    let a = style.split(' ');
+//    a.forEach(s => {
+//        if (message.length > 20)
+//            li.classList.add("long-message")
+//
+//        li.classList.add(s)
+//    });
+//
+//    arr.appendChild(li);
+//}
+
+
+
+function MessageCreater(message, style, author, date, avatarUrl) {
+    let chatBubbles = document.querySelector(".chat-bubbles"); 
+
+    if (!chatBubbles) {
+        //elem yoxdsa yaradr
+        chatBubbles = document.createElement("div");
+        chatBubbles.classList.add("chat-bubbles");
+        document.body.appendChild(chatBubbles);
+    }
+
+    
+    let chatItem = document.createElement("div");
+    chatItem.classList.add("chat-item");
+
+    chatItem.innerHTML = `
+        <div class="row align-items-end justify-content-end">
+            <div class="col col-lg-6">
+                <div class="chat-bubble ${style}">
+                    <div class="chat-bubble-title">
+                        <div class="row">
+                            <div class="col chat-bubble-author">${author}</div>
+                            <div class="col-auto chat-bubble-date">${date}</div>
+                        </div>
+                    </div>
+                    <div class="chat-bubble-body">
+                        <p>${message}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-auto">
+                <span class="avatar" style="background-image: url(${avatarUrl})"></span>
+            </div>
+        </div>
+    `;
+
+    chatBubbles.appendChild(chatItem);
 }
+
 //#endregion
